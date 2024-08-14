5425f8960bd0505db950c21166736f7cb7406181
Jorgon29 <00016823@uca.edu.sv>
2024-08-14 13:57:03 -0600

    Agregada autocompletion

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,12 +10,19 @@ return
 			"williamboman/mason-lspconfig.nvim",
 			config = function()
 			require("mason-lspconfig").setup({
-					ensure_installed = { "lua_ls", "clangd", "quick_lint_js"}
+					ensure_installed = { "lua_ls", "clangd", "quick_lint_js", "rust_analyzer", "html"}
 				})
 			end
 			
 		},
 		{
-			"neovim/nvim-lspconfig"
+			"neovim/nvim-lspconfig",
+			config = function()
+				local lspconfig = require("lspconfig")
+				local capabilities = require('cmp_nvim_lsp').default_capabilities()
+				lspconfig.html.setup({
+					capabilities = capabilities
+				})
+			end
 		},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "java", "cpp" },
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "java", "cpp", "rust", "css"},
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },  
